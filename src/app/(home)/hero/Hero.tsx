738e9c2e33ed107b33a91a98e9a7455923a8d659
Eunice <eunice.itcosmed@gmail.com>
2024-08-01 17:18:12 +0100

    hero added

--- src/app/(home)/hero/Hero.tsx
+++ src/app/(home)/hero/Hero.tsx
@@ -1,55 +1,61 @@
 import React from "react";
 import Image from "next/image";
+import Container from "@/components/container/Container";
 
 const Hero = () => {
   return (
     <section className="pt-32">
-      <div
-        className=" container h-72 flex justify-between items-center
+      <Container>
+        <div
+          className="h-72  text-white flex justify-between items-center
              bg-secondary-500"
-      >
-        <div
-          className=" flex-1 md:flex-none justify-between md:justify-start
-        text-white gap-40 md:gap-8 flex items-center"
         >
-          <Image
-            width={100}
-            height={200}
-            src="/assets/homeGraphic1.png"
-            alt="Banner yellow image"
-          />
-          <p className=" text-xl md:text-3xl">
-            <span className=" text-pink-600">HOT</span>
-            SALES <br /> WEEK
-          </p>
-        </div>
+          <div
+            className="flex-1  justify-between md:justify-start
+        text-white gap-8 flex items-center"
+          >
+            <Image
+              width={100}
+              height={200}
+              src="/assets/homeGraphic1.png"
+              alt="Banner yellow image"
+            />
 
-        <div
-          className=" relative h-52 w-52 bg-white hidden rounded-full md:flex
+            <p className="block md:hidden Lg:block text-xl md:text-3xl">
+              <span className=" text-pink-600">HOT</span>
+              SALES <br /> WEEK
+            </p>
+          </div>
+
+          <div
+            className=" relative h-52 w-52 bg-white hidden rounded-full md:flex
              justify-center items-center before:content-[''] before:absolute before:top-[8px]
               before:right-[8px] before:bottom-[8px] before:left-[8px] before:rounded-full before:border-4
                before:border-dashed before:border-black"
-        >
-          <p
-            className="text-black text-3xl
-                     font-bold opacity-80"
           >
-            50% <br /> OFF
-          </p>
+            <p
+              className="text-black text-3xl
+                     font-bold opacity-80"
+            >
+              50% <br /> OFF
+            </p>
+          </div>
+          <div className="hidden md:flex items-center gap-8">
+            <p className=" text-white hidden Lg:block  text-xl md:text-3xl">
+              A NEW <span className=" text-pink-500">GREAT LOOK</span>
+            </p>
+            <p className=" text-[#867EBC] text-xl md:text-3xl">
+              FOR NEW SEASON
+            </p>
+          </div>
+          <Image
+            width={100}
+            height={200}
+            src="/assets/homeGraphic3.png"
+            alt="Banner black image"
+          />
         </div>
-        <div className="hidden md:flex items-center gap-8">
-          <p className=" text-white hidden Lg:block  text-xl md:text-3xl">
-            A NEW <span className=" text-pink-500">GREAT LOOK</span>
-          </p>
-          <p className=" text-[#867EBC] text-xl md:text-3xl">FOR NEW SEASON</p>
-        </div>
-        <Image
-          width={100}
-          height={200}
-          src="/assets/homeGraphic3.png"
-          alt="Banner black image"
-        />
-      </div>
+      </Container>
     </section>
   );
 };
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import { BsCart3 } from "react-icons/bs";
 import Badge from "../badge/Badge";
 import { MdClose } from "react-icons/md";
 import { RxHamburgerMenu } from "react-icons/rx";
-
+import Container from "../container/Container";
 const styles =
   "relative before:content-[' '] before:w-[80%] before:h-1 before:bg-amber-700 before:scale-0 before:left-0 before:duration-500 before:transition-all before:bottom-[-4px] before:absolute hover:before:scale-100 before:origin-left";
 
@@ -14,74 +14,83 @@ const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
   return (
     <header
-      className=" fixed w-full py-5 h-[80px] z-40 text-white
+      className=" fixed w-full py-5 h-
+      [80px] z-40 text-white
      bg-primary-700 gap-5"
     >
-      <div className=" container flex justify-between items-center gap-5 font-bold">
-        <div className=" z-10 md:hidden" onClick={() => setShowMenu(!showMenu)}>
-          {showMenu ? <MdClose size={30} /> : <RxHamburgerMenu size={30} />}
-        </div>
-        <div
-          className="text-3xl font-bold z-10
+      <Container>
+        <div className="flex justify-between items-center gap-5 font-bold">
+          <div
+            className=" z-10 md:hidden"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            {showMenu ? <MdClose size={30} /> : <RxHamburgerMenu size={30} />}
+          </div>
+          <div
+            className="text-3xl font-bold z-10
         "
-        >
-          MIKE
-          <span className=" text-accent-500">SHOP</span>
-        </div>
-        <form className="flex">
-          <input
-            type="text"
-            placeholder="search"
-            className="py-2 px-2 border-none
+          >
+            MIKE
+            <span className=" text-accent-500">SHOP</span>
+          </div>
+          <form className="flex">
+            <input
+              type="text"
+              placeholder="search"
+              className="py-2 px-2 border-none
            w-full md:w-[300px]"
-          />
-          <button
-            type="submit"
-            className="
+            />
+            <button
+              type="submit"
+              className="
              bg-accent-400 py-2 
              px-5 border-none cursor-pointer"
-          >
-            search
-          </button>
-        </form>
-        <div
-          className=" flex items-center
+            >
+              search
+            </button>
+          </form>
+          <div
+            className=" flex items-center
          gap-5"
-        >
-          <nav
-            className={`w-full md:w-auto h-screen md:h-auto
+          >
+            <nav
+              className={`w-full md:w-auto h-screen md:h-auto
           absolute md:static top-0 bg-secondary-500 md:bg-transparent transition-all
           duration-300 ease-in 
           ${showMenu ? "left-0" : "left-[-100%]"}`}
-          >
-            <ul
-              className="flex flex-col md:flex-row md:items-center mt-24 md:mt-0  px-5 md:px-0 
-              gap-5 text-2xl font-bold md:text-base text-black md:text-white"
             >
-              <li className="{`${styles}`}" onClick={() => setShowMenu(false)}>
-                <Link href="/">Home</Link>
-              </li>
-              <li
-                className=" {`${styles}`}
-              onClick={() => setShowMenu(false)}"
+              <ul
+                className="flex flex-col md:flex-row md:items-center mt-24 md:mt-0  px-5 md:px-0 
+              gap-5 text-2xl font-bold md:text-base text-black md:text-white"
               >
-                <Link href="/register">Register</Link>
-              </li>
-              <li
-                className="{`${styles}`}
+                <li
+                  className="{`${styles}`}"
+                  onClick={() => setShowMenu(false)}
+                >
+                  <Link href="/">Home</Link>
+                </li>
+                <li
+                  className=" {`${styles}`}
               onClick={() => setShowMenu(false)}"
-              >
-                <Link href="/login">Login</Link>
-              </li>
-            </ul>
-          </nav>
-          <Badge content={3}>
-            <Link href="/cart">
-              <BsCart3 size={30} color="white" />
-            </Link>
-          </Badge>
+                >
+                  <Link href="/register">Register</Link>
+                </li>
+                <li
+                  className="{`${styles}`}
+              onClick={() => setShowMenu(false)}"
+                >
+                  <Link href="/login">Login</Link>
+                </li>
+              </ul>
+            </nav>
+            <Badge content={3}>
+              <Link href="/cart">
+                <BsCart3 size={30} color="white" />
+              </Link>
+            </Badge>
+          </div>
         </div>
-      </div>
+      </Container>
     </header>
   );
 };

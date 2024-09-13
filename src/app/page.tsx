699fb53c507652159191e037c2fd4a9e46374615
Eunice <eunice.itcosmed@gmail.com>
2024-09-13 19:47:00 +0100

    Added phones and latestProducts section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Hero from "./(home)/hero/Hero";
 import HotProducts from "./(home)/hotProducts/HotProducts";
+import Categories from "./(home)/categories/Categories";
+import LatestProducts from "./(home)/latestProducts/LatestProducts";
+import Phone from "./(home)/phone/Phone";
 
 const Home = () => {
   return (
     <main>
       <Hero />
       <HotProducts />
+      <Categories />
+      <LatestProducts />
+      <Phone />
     </main>
   );
 };
--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -2,17 +2,26 @@ import Image from "next/image";
 import React from "react";
 import Button from "../button/Button";
 import { IItem } from "@/types";
+import { SiHootsuite } from "react-icons/si";
+
 interface productProps {
   item: IItem;
+  showBlackButton?: boolean;
+  objectCover?: boolean;
 }
 
-const Product = ({ item }: productProps) => {
+const Product = ({ item, showBlackButton, objectCover }: productProps) => {
   return (
-    <div className="w-full p-5 bg-secondary-200">
+    <div className=" flex-1 shadow-lg bg-secondary-200">
       <div className="w-full h-52 relative">
-        <Image fill src={item.img} alt="Product image" />
+        <Image
+          fill
+          src={item.img}
+          alt="Product image"
+          className={objectCover ? "object-cover" : "object-contain"}
+        />
       </div>
-      <div className="w-full py-5">
+      <div className="w-full p-5">
         <div className="flex justify-between gap-2">
           <span className="font-bold line-clamp-1 ">{item.title}</span>
           <span className="font-bold text-accent-500">${item.price}</span>
@@ -22,9 +31,11 @@ const Product = ({ item }: productProps) => {
           <Button size="small" type="primary">
             More Details
           </Button>
-          <Button size="small" type="neutral">
-            Add to Cart
-          </Button>
+          {showBlackButton && (
+            <Button type="neutral" size="small">
+              Add To Cart
+            </Button>
+          )}
         </div>
       </div>
     </div>
